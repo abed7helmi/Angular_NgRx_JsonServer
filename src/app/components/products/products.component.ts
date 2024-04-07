8126126a987dc8d807b42a88d6fb96fc5209ef69
abed7helmi <abed.helmi@yahoo.fr>
2024-04-07 03:21:36 +0200

    show all products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductsService} from "../../services/products.service";
+import {Product} from "../../model/product.model";
 
 @Component({
   selector: 'app-products',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products : Product[] | null =null
+
+  constructor(private productsService : ProductsService) { }
 
   ngOnInit(): void {
   }
 
+  onGetAllProducts() {
+    /*this.products$= this.productsService.getAllProducts().pipe(
+      map(data=>{
+        console.log(data);
+        return ({dataState:DataStateEnum.LOADED,data:data})
+      }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(err=>of({dataState:DataStateEnum.ERROR, errorMessage:err.message}))
+    );*/
+    this.productsService.getAllProducts().subscribe(
+      data =>{
+        this.products=data
+      }
+    )
+  }
+
+  onGetSelectedProducts() {
+
+  }
+
+  onGetAvailableProducts() {
+
+  }
+
+  onNewProduct() {
+
+  }
 }
